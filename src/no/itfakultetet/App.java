4f928193ac41c8dea7ad0c26387f92b8fd90dee6
terjebh <terje@itfakultetet.no>
2023-05-31 12:51:27 +0200

    Liste lagt til

--- src/no/itfakultetet/App.java
+++ src/no/itfakultetet/App.java
@@ -8,8 +8,6 @@ public class App {
 
     public static void main(String[] args) {
 
-
-
         Laptop l1 = new Laptop();
         l1.setType("Lenovo Thinkpad X240");
         l1.setModell(2017);
@@ -21,24 +19,32 @@ public class App {
 
         Laptop l3 = new Laptop("Lenovo Thinkpad w541");
 
-        System.out.println("Laptop 1 er en : "+l1.getType() + " fra "+l1.getModell());
-        System.out.println(l2);
-        System.out.println(l3);
+       // System.out.println("Laptop 1 er en : "+l1.getType() + " fra "+l1.getModell());
+       // System.out.println(l2);
+       //  System.out.println(l3);
 
 
         Server s1 = new Server(4);
-        s1.setType("Dell Poweredge r720");
+        s1.setType("Dell Poweredgde r720");
         s1.setModell(2018);
 
         Desktop d1 = new Desktop();
         d1.setType("Lenovo Thinkcentre");
 
+        /*
         System.out.println("Antall Laptoper er nå: " + Laptop.antallLaptoper);
         System.out.println("Antall Desktoper er nå: " + Desktop.antallDesktoper);
         System.out.println("Antall Servere er nå: " + Server.antallServere);
-
         System.out.println("Antall Datamaskiner totalt er nå: "+Datamaskin.antallMaskiner);
+        */
 
+        List<Datamaskin> maskiner = new ArrayList<>();
+
+        maskiner.add(l1);
+        maskiner.add(d1);
+        maskiner.add(s1);
+
+        maskiner.forEach(System.out::println);
 
 
 
